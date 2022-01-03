1ed1f9cdc09c8dc1a9af716cd8724a1e3e6bd33f
user <user@example.com>
2022-01-03 20:40:59 +0100

    change hello world to make sure it uses scala3

--- src/main/scala/TutorialApp.scala
+++ src/main/scala/TutorialApp.scala
@@ -1,7 +1,8 @@
 package com.example
 
 object TutorialApp {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     println("Hello world!")
-  }
+    println("Scala 3 has meaningful whitespaces!")
+
 }
